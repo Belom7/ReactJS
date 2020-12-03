5ab831ea0b6ec4e8eef7dad57443a736f26b0363
Belom7 <Still07pkm@gmail.com>
2020-12-03 23:07:47 +0300

    сделал 3 задание частично

--- main.js
+++ main.js
@@ -23,3 +23,56 @@ const calculateArea = (width, height) => {
 };
 
 console.log(calculateArea(4,5))
+
+/* 3. Необходимо написать иерархию классов вида:
+Human -> Employee -> Developer
+Human -> Employee -> Manager
+Каждый Менеджер (Manager) должен иметь внутренний массив своих сотрудников (разработчиков), а также методы по удалению/добавлению разработчиков.
+Каждый Разработчик (Developer) должны иметь ссылку на Менеджера и методы для изменения менеджера (имеется ввиду возможность назначить другого менеджера).
+У класса Human должны быть следующие параметры: name (строка), age (число), dateOfBirth (строка или дата)
+У класса Employee должны присутствовать параметры: salary (число), department (строка)
+В классе Human должен присутствовать метод displayInfo, который возвращает строку со всеми параметрами экземпляра Human.
+В классе Employee должен быть реализовать такой же метод (displayInfo), который вызывает базовый метод и дополняет его параметрами из экземпляра Employee
+Чтобы вызвать метод базового класса, необходимо внутри вызова метода displayInfo класса Employee написать: super.displayInfo(), это вызовет метод disaplyInfo класс Human и вернет строку с параметрами Human'a. */
+
+class Human {
+    constructor(name,age,dateOfBirth) {
+        this.name = name, 
+        this.age = age,
+        this.dateOfBirth = dateOfBirth
+    }
+    displayInfo() {
+        return [this.name, this.age, this.dateOfBirth]
+    }
+}
+
+const human = new Human('max',29,1991)
+console.log(human.displayInfo())
+
+// class Employee extends Human {
+//     constructor(name, age, dateOfBirth, salary, departament) {
+//         super(name, age, dateOfBirth),
+//         this.salary = salary,
+//         this.departament = departament
+//     }
+//     displayInfo() {
+//         return [...super.displayInfo(), this.salary, this.departament]  
+//     }        
+// }
+
+// // class Developer {
+// //     constructor() {}
+// // }
+
+// // class Manager {
+// //     constructor() {
+// //         let Arr = []
+// //     }
+// // }
+
+
+// // const human = new Human('Max',29,1991)
+// // console.log(human.displayInfo())
+
+// const employee = new Employee('Max',29,1991,4,'xxxx')
+// console.log(employee.displayInfo())
